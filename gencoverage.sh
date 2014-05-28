7bbfc0c5924a3d867b495aa0de32238d792430a5
swarminglogic <github@swarminglogic.com>
2014-05-28 23:54:59 +0200

    Added option to ./gencoverage.sh to skip compilation
    Added option to ./gencoverage.sh to skip compilation
    This is useful when only the tests are being modified.
    It is then not necessary to recompile the whole project from scratch.
    Usage: `./gencoverage.sh --update`
    
        gencoverage.sh

--- gencoverage.sh
+++ gencoverage.sh
@@ -1,33 +1,41 @@
 #!/bin/bash
 
+update=
+[[ "$1" == '--update' ]] && update=true
+
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`/lib
 export LDFLAGS="-lgcov -fprofile-arcs"
 export CPPFLAGS="-fprofile-arcs -ftest-coverage"
 
-# Cleaning all build files
-echo y | ./devtools.sh cleanl
+if [[ ! $update ]] ; then
+    # Cleaning all build files
+    echo y | ./devtools.sh cleanl
 
-# Compiling whole project, including tests
-./compile.sh l -t
+    # Compiling whole project, including tests
+    ./compile.sh l -t
+else
+    # Remove existing runtime line coverage files
+    find ./build -name "*.gcda" -exec rm {} \;
+fi
 
 # Generating initial coverage information
-lcov -b . -c -i -d ./build -o .coverage.wtest.base
+lcov -q -b . -c -i -d ./build -o .coverage.wtest.base
 
 # Running tests
 for i in ./bin/tests/* ; do $i ; done
 
 # Generating coverage based on executed tests
-lcov -b . -c -d ./build -o .coverage.wtest.run
+lcov -q -b . -c -d ./build -o .coverage.wtest.run &> /dev/null
 
 # Mergin coverage tracefiles
-lcov -b . -a .coverage.wtest.base -a .coverage.wtest.run  -o .coverage.total
+lcov -a .coverage.wtest.base -a .coverage.wtest.run  -o .coverage.total
 
 # Filtering, extracting project files
-lcov -b . -e .coverage.total "`pwd`/*" -o .coverage.total.filtered
+lcov -q -e .coverage.total "`pwd`/*" -o .coverage.total.filtered
 
 # Filtering, removing TestFiles and main.cpp
-lcov -b . -r .coverage.total.filtered "`pwd`/build/main.cpp" -o .coverage.total.filtered
-lcov -b . -r .coverage.total.filtered "`pwd`/*/Test*.*" -o .coverage.total.filtered
+lcov -q -r .coverage.total.filtered "`pwd`/build/main.cpp" -o .coverage.total.filtered
+lcov -q -r .coverage.total.filtered "`pwd`/*/Test*.*" -o .coverage.total.filtered
 
 # Replacing /build/ with /src/ to unify directories
 sed 's/\/build\//\/src\//g' .coverage.total.filtered > .coverage.total
